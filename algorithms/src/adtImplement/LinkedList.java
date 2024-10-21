24e288bd9ddbe97a48799b3bf0efc5cbc540b23e
Dorg <32146176+YakuzaDoggo@users.noreply.github.com>
2024-10-21 19:34:53 -0400

    Add files via upload

--- algorithms/src/adtImplement/LinkedList.java
+++ algorithms/src/adtImplement/LinkedList.java
@@ -5,24 +5,23 @@ public class LinkedList {
 	
 	class Node{
 		int x;
-		String y;
 		Node next;
 	}
 	
 	private Node head;
 	private int length;
 	
-	public LinkedList() {
-		this.head = null;
-		this.length = 0;
+	public LinkedList(int x) {
+		this.head.x = x;
+		this.head.next = null;
+		this.length = 1;
 	}
 	
 	// add to end of list
-	private void add(int x, String y) {
+	private void add(int x) {
 		// create new node
 		Node anode = new Node();
 		anode.x = x;
-		anode.y = y;
 		anode.next = null;
 		
 	
@@ -47,10 +46,17 @@ public class LinkedList {
 		
 	}
 	
+	
 	private Node getNodeAt(int pos) {
-		Node reader = head;
-		// start at second node (so technically the first node with actual data)
-		int cnt = 1;
+		if(pos == 1) {
+			return this.head;
+		}
+		if(pos > this.length || pos <= 0) {
+			return null;
+		}
+		Node reader = this.head;
+		// start at second node (so the one after the head)
+		int cnt = 2;
 		while(cnt < pos) {
 			// move through the list
 			reader = reader.next;
@@ -62,25 +68,24 @@ public class LinkedList {
 	
 	// add to arbitrary position in list
 	private void add(int x, String y, int pos) {
-		if(pos > this.length) {
-			this.add(x, y);
+		if(pos > this.length || pos <= 0) {
+			this.add(x);
 		}
 		Node anode = new Node();
 		anode.x = x;
-		anode.y = y;
-		
+
 		// find node that is BEFORE the current position
 		// set new Node next to be node in current position
 		// set Node before current position to point to new node
 		
 		// if the current value to change is the second node (the first actual element)
 		if(pos == 1) {
-			anode.next = this.head.next;
-			this.head.next = anode;
+			anode.next = this.head;
+			this.head = anode;
 		}
 		else {
 			// find the node BEFORE the one we actually want to change
-			Node ndebefore = this.getNodeAt(pos);
+			Node ndebefore = this.getNodeAt(pos - 1);
 			// position - 1 has been found
 			anode.next = ndebefore.next;
 			ndebefore.next = anode;
@@ -94,14 +99,19 @@ public class LinkedList {
 	
 	
 	private void remove(int pos) {
-		
+		if(pos > this.length || pos <= 0) {
+			return;
+		}
 		// does the first element need to be removed?
 		if(pos == 1) {
-			this.head.next = this.head.next.next;
+			Node temp = this.head.next;
+			Node next = temp.next;
+			this.head = temp;
+			this.head.next = next;
 		}
 		else {
 			// get position before removed node
-			Node reader = getNodeAt(pos);
+			Node reader = getNodeAt(pos - 1);
 			
 			// get postion after removed node
 			Node newnxt = reader.next.next;
@@ -114,21 +124,22 @@ public class LinkedList {
 		
 	}
 	
-	private void update(int x, String y, int pos) {
-		
-		if(pos == 1) {
-			this.head.next.x = x;
-			this.head.next.y = y;
+	private void update(int x, int pos) {
+		// if pos is invalid, do nothing
+		if(pos > this.length || pos <= 0) {
+			return;
 		}
+		
+		// is position requested is 1, just work directly on the head
+		if(pos == 1) {
+			this.head.x = x;
+		}
+		// get the node after 
 		else {
 			Node changed = this.getNodeAt(pos).next;
 			changed.x = x;
-			changed.y = y;
 		}
 		
 	}
-	
-	
-	
 	
 }
